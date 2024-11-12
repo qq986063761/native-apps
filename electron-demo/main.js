7107ae9a2e1acf24ac8bdedff5b6804165ad5550
wanpeng <986063761@qq.com>
2024-11-12 20:48:24 +0800

    更新

--- electron-demo/main.js
+++ electron-demo/main.js
@@ -10,4 +10,17 @@ const createWindow = () => {
   win.loadFile('index.html')
 }
 
-app.whenReady().then(createWindow)
+// 事件
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+// 应用进程准备完毕
+app.whenReady().then(() => {
+  createWindow()
+
+  // 激活后，如果窗口打开失败，则重新打开
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
